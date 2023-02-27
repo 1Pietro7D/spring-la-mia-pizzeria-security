00f1bb8c5d4f6ac3404d1934338a7a2db14e34d9
PietroDanieli <1pie7d@gmail.com>
2023-02-27 09:26:55 +0100

    implement method show

--- src/main/java/org/feb14/pizzeria/api/controller/PizzaApiController.java
+++ src/main/java/org/feb14/pizzeria/api/controller/PizzaApiController.java
@@ -1,11 +1,15 @@
 package org.feb14.pizzeria.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.feb14.pizzeria.model.Pizza;
 import org.feb14.pizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,4 +28,14 @@ public class PizzaApiController {
 			return pizzaRepository.findAll();
 		}
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Pizza> show(@PathVariable("id") Integer id) {
+		Optional<Pizza> result = pizzaRepository.findById(id);
+		if (result.isPresent())
+			return new ResponseEntity<Pizza>(result.get(), HttpStatus.OK);
+		else
+			return new ResponseEntity<Pizza>(HttpStatus.NOT_FOUND);
+	}
+
 }
